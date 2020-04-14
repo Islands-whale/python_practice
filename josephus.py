b5890c070a3bb973dab575505b1702856caf4d76
zcs730 <chongsen_zhao@163.com>
2020-04-15 00:37:18 +0800

    添加类(class Person)，对其对象进行约瑟夫排序

--- josephus.py
+++ josephus.py
@@ -35,9 +35,54 @@ def cycle_sort(target, begin, step):
     return result
 
 
-if __name__ == "__main__":
-    original = ['a', 'b', 'c', 'd']
-    final = cycle_sort(original, 2, 2)
-    print(final)
+class Person:
+    """Summary of class here.
+
+    人的基本信息，包括姓名、年龄
+
+    Attributes:
+        __name: 姓名
+        __age: 年龄
+    """
+    def __init__(self, name, age):
+        """Inits SampleClass with blah."""
+        self.__name = name
+        self.__age = age
+        # print("name:", self.__name, ": constructor")
+
+    # def __del__(self):
+    #     """Destructor."""
+    #     print("name:", self.__name, ": destructor")
+
+    def print_data(self):
+        """Export sales information."""
+        print("name:", self.__name, "\tage:", self.__age)
+
+
+if __name__ == '__main__':
+
+    Person1 = Person("zhangsan", 30)
+    Person2 = Person("lisi", 35)
+    Person3 = Person("wangwu", 42)
+    Person4 = Person("zhouliu", 10)
+
+    original = [Person1, Person2, Person3, Person4]
+    print("\nThe sequence before sorting is:\n")
+    for i in range(len(original)):
+        original[i].print_data()
+
+    try:
+        begin = int(input("\nPlease input a starting position:"))
+        step = int(input("Please input a step number:"))
+    except ValueError:
+        print("\nPlease input an integer!")
+    else:
+        if begin <= 0 or begin > len(original) or step <= 0:
+            raise IndexError("Out of range!")
+
+        final = cycle_sort(original, begin, step)
+        print("\nThe sequence after sorting is:\n")
+        for i in range(len(final)):
+            final[i].print_data()
 
 # *=====End File=====* #
