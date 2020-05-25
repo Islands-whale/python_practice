7449a3f0ebb50040986047b357091373eabd06a5
zcs730 <chongsen_zhao@163.com>
2020-05-25 15:34:43 +0800

    界面与逻辑分离

--- src/user_interface/call_qt_graphic_interface.py
+++ src/user_interface/call_qt_graphic_interface.py
@@ -2,13 +2,12 @@ import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QFileDialog, QInputDialog,
                              QMessageBox)
 from src.user_interface.qt_graphic_interface import QtUiForm
-from src.josephus.josephus import RingSort
-from src.file_reader.file_reader import ReaderFactory, ZipReader
-from os.path import splitext
-from zipfile import ZipFile
+from src.adapters.file_reader import ZipReader
+from src.adapters.file_reader_use_case import FileReader
+from src.use_cases.josephus_use_case import JosephusUseCase
 
 
-class CallQtUiForm(QWidget, QtUiForm):
+class UiForm(QWidget, QtUiForm):
     def __init__(self):
         super().__init__()
         self.setup_ui(self)
@@ -17,64 +16,51 @@ class CallQtUiForm(QWidget, QtUiForm):
         self.confirm_button.clicked.connect(self.show_ring)
 
     def choose_file(self):
-        get_file_path, ok = QFileDialog.getOpenFileName(self, '请选择文件')
+        file_path, ok = QFileDialog.getOpenFileName(self, '请选择文件')
         if ok:
-            self.path_edit.setText(get_file_path)
-
-    def from_zip(self, path):
-        try:
-            with ZipFile(path) as fp:
-                members = fp.namelist()
-            member, ok = QInputDialog.getItem(self, '选择文件', '请选择文件', members)
-            if ok:
-                return ZipReader(path, member).next()
-
-            return None
-
-        except IOError:
-            return None
-
-    def from_txt_csv(self, path):
-        reader = ReaderFactory.get_reader(self.file_type)
-        if reader:
-            if next(reader(path)) is None:
-                return None
-
-            return reader(path)
-
-        return None
+            self.path.setText(file_path)
 
     def show_ring(self):
         self.out_browser.clear()
-        self.file_type = splitext(self.path_edit.text())[-1]
-        if self.file_type == '.zip':
-            reader_generator = self.from_zip(self.path_edit.text())
+        path = self.path.text()
 
-        else:
-            reader_generator = self.from_txt_csv(self.path_edit.text())
-
-        try:
-            assert reader_generator is not None
-        except Exception:
+        ok = FileReader(path).check_path()
+        if not ok:
             QMessageBox.critical(self, '错误', '文件路径错误，请重新选择！', QMessageBox.Yes)
+
         else:
-            try:
-                assert int(self.start_edit.text()) > 0
-                assert int(self.step_edit.text()) > 0
-            except Exception:
-                QMessageBox.critical(self, '错误', '请输入正整数！', QMessageBox.Yes)
+            yes = FileReader(path).check_type()
+            if yes:
+                members = ZipReader.get_file_list(path)
+                current_file, ok = QInputDialog.getItem(
+                    self, '选择文件', '请选择文件', members)
+                if ok:
+                    reader_generator = FileReader(path).from_zip(current_file)
 
             else:
-                ring = RingSort(start=int(self.start_edit.text()),
-                                step=int(self.step_edit.text()),
-                                reader=reader_generator)
+                reader_generator = FileReader(path).from_txt_csv()
 
-                for i in ring:
-                    self.out_browser.append(i.get_information())
+            if not reader_generator:
+                QMessageBox.critical(self, '错误', '文件路径错误，请重新选择！',
+                                     QMessageBox.Yes)
+
+            else:
+                ok = JosephusUseCase.check_number(self.start.text(),
+                                                  self.step.text())
+                if not ok:
+                    QMessageBox.critical(self, '错误', '请输入正整数！',
+                                         QMessageBox.Yes)
+
+                else:
+                    ring_iterator = JosephusUseCase.create_josephus(
+                        self.start.text(), self.step.text(), reader_generator)
+
+                    for each in ring_iterator:
+                        self.out_browser.append(str(each))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    interface = CallQtUiForm()
+    interface = UiForm()
     interface.show()
     sys.exit(app.exec_())
