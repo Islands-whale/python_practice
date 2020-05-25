4087679a6b50c2b6857f5ca862926bdaf2460867
zcs730 <chongsen_zhao@163.com>
2020-05-25 15:43:06 +0800

    添加文件读取用例，判断用户输入是否正确

---
+++ src/adapters/file_reader_use_case.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' File reader use cace module '
+
+__author__ = 'Chongsen Zhao'
+
+import os.path
+from src.adapters.file_reader import ReaderFactory, ZipReader
+
+
+class FileReader:
+
+    def __init__(self, path):
+        self.path = path
+        self.type = os.path.splitext(path)[-1]
+        self.reader = ReaderFactory.get_reader(self.type)
+
+    def check_path(self):
+        if self.type not in ['.txt', '.csv', '.zip']:
+            return None
+
+        return bool(self.reader(self.path))
+
+    def check_type(self):
+        return self.type == '.zip'
+
+    def from_zip(self, member):
+        return ZipReader.from_zip(self.path, member)
+
+    def from_txt_csv(self):
+        return next(self.reader(self.path))
