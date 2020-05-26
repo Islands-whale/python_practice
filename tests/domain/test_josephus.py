be9def6adf8f33a925fdb435c3d5bcdc16797ac4
zcs730 <chongsen_zhao@163.com>
2020-05-26 12:52:40 +0800

    使用mock模拟reader迭代器

--- tests/domain/test_josephus.py
+++ tests/domain/test_josephus.py
@@ -6,18 +6,21 @@
 __author__ = 'Chongsen Zhao'
 
 import unittest
+from unittest.mock import MagicMock
 from src.domain.josephus import RingSort
 
 
 class TestRingSort(unittest.TestCase):
 
     def setUp(self):
-        self.ring = RingSort(1, 1, ['1', '2', '3'])
+        mock = MagicMock()
+        mock.__iter__.return_value = [1, 2, 3]
+        self.ring = RingSort(1, 1, mock)
 
     def test_init(self):
         self.assertEqual(self.ring.current_id, 0)
         self.assertEqual(self.ring.step, 1)
-        self.assertEqual(self.ring._people, ['1', '2', '3'])
+        self.assertEqual(self.ring._people, [1, 2, 3])
 
     def test_len(self):
         self.assertEqual(len(self.ring), 3)
@@ -26,16 +29,16 @@ class TestRingSort(unittest.TestCase):
         self.assertTrue(self.ring < 4)
 
     def test_append(self):
-        self.ring.append('4')
-        self.assertEqual(self.ring._people, ['1', '2', '3', '4'])
+        self.ring.append(4)
+        self.assertEqual(self.ring._people, [1, 2, 3, 4])
 
     def test_iter(self):
         self.assertEqual(iter(self.ring), self.ring)
 
     def test_next(self):
-        self.assertEqual(next(self.ring), '1')
-        self.assertEqual(next(self.ring), '2')
-        self.assertEqual(next(self.ring), '3')
+        self.assertEqual(next(self.ring), 1)
+        self.assertEqual(next(self.ring), 2)
+        self.assertEqual(next(self.ring), 3)
 
         with self.assertRaises(StopIteration):
             next(self.ring)
