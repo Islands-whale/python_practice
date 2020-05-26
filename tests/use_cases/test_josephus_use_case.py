eaa27d19a1d2ce041ca9ea42d72e157d93ec5c1c
zcs730 <chongsen_zhao@163.com>
2020-05-26 17:41:36 +0800

    使用mock模拟创建约瑟夫环

--- tests/use_cases/test_josephus_use_case.py
+++ tests/use_cases/test_josephus_use_case.py
@@ -6,6 +6,7 @@
 __author__ = 'Chongsen Zhao'
 
 import unittest
+from unittest.mock import MagicMock
 from src.use_cases.josephus_use_case import JosephusUseCase
 
 
@@ -17,10 +18,12 @@ class TestJosephusUseCase(unittest.TestCase):
         self.assertFalse(JosephusUseCase.check_number('1', 'a'))
 
     def test_create_josephus(self):
-        ring = JosephusUseCase.create_josephus('1', '1', ['a', 'b'])
+        mock = MagicMock()
+        mock.__iter__.return_value = [1, 2, 3]
+        ring = JosephusUseCase.create_josephus('1', '1', mock)
         self.assertEqual(ring.current_id, 0)
         self.assertEqual(ring.step, 1)
-        self.assertEqual(ring._people, ['a', 'b'])
+        self.assertEqual(ring._people, [1, 2, 3])
 
 
 if __name__ == '__main__':
