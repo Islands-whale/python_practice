2846ef0beace39b5646fcc7e43c3ba1bbf5e3d44
zcs730 <chongsen_zhao@163.com>
2020-05-26 18:28:47 +0800

    修改文字界面

--- src/user_interface/text_interface.py
+++ src/user_interface/text_interface.py
@@ -1,83 +1,75 @@
 import curses
-from src.josephus.josephus import RingSort
-from src.file_reader.file_reader import ReaderFactory, ZipReader
-from os.path import splitext
+from src.user_interface.file_reader_use_case import FileReader
+from src.use_cases.josephus_use_case import JosephusUseCase
 
 
-class TextUiForm:
+class UiForm:
     def __init__(self):
         self.screen = curses.initscr()
-        self.cursor_y = 3
+        self.cursor_y = 5
         curses.start_color()
         curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
         curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
         curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
-    def get_data(self):
+    def show_ring(self):
         self.screen.addstr(0, 0, "Please input a file path:",
                            curses.color_pair(3))
-        path = self.screen.getstr()
-        self.file_type = splitext(path)[-1]
-        if self.file_type == '.zip':
-            reader_generator = self.from_zip(path)
+        path = self.screen.getstr().decode()
 
-        else:
-            reader_generator = self.from_txt_csv(path)
-
-        try:
-            assert reader_generator is not None
-        except Exception:
+        ok = FileReader(path).check_path()
+        if not ok:
             self.screen.addstr(4, 0, "Wrong: File path error!",
                                curses.color_pair(1))
+
         else:
-            try:
-                self.screen.addstr(2, 0, "Please input a starting position:",
+            yes = FileReader(path).check_zip()
+            if yes:
+                self.screen.addstr(1, 0, "Please input a file member:",
                                    curses.color_pair(3))
-                start = self.screen.getstr()
-                assert int(start) > 0
-                self.screen.addstr(3, 0, "Please input a step number:",
-                                   curses.color_pair(3))
-                step = self.screen.getstr()
-                assert int(step) > 0
-            except Exception:
-                self.screen.addstr(4, 0,
-                                   "Wrong: Please input positive integer!",
+                current_file = self.screen.getstr().decode()
+                reader_generator = FileReader(path).from_zip(current_file)
+
+            else:
+                reader_generator = FileReader(path).from_txt_csv()
+
+            if not reader_generator:
+                self.screen.addstr(4, 0, "Wrong: File path error!",
                                    curses.color_pair(1))
 
             else:
-                ring = RingSort(start, step, reader_generator)
+                self.screen.addstr(2, 0, "Please input a starting position:",
+                                   curses.color_pair(3))
+                start = self.screen.getstr()
 
-                self.screen.addstr(3, 0, "The sequence after sorting is:",
-                                   curses.color_pair(2))
-                for i in ring:
-                    self.cursor_y += 1
-                    self.screen.addstr(self.cursor_y, 0, i.get_information(),
+                self.screen.addstr(3, 0, "Please input a step number:",
+                                   curses.color_pair(3))
+                step = self.screen.getstr()
+
+                ok = JosephusUseCase.check_number(start, step)
+                if not ok:
+                    self.screen.addstr(
+                        4, 0, "Wrong: Please input positive integer!",
+                        curses.color_pair(1))
+
+                else:
+                    ring_iterator = JosephusUseCase.create_josephus(
+                        start, step, reader_generator)
+
+                    self.screen.addstr(5, 0, "The sequence after sorting is:",
                                        curses.color_pair(2))
-        finally:
-            self.screen.addstr(curses.LINES - 1, 0,
-                               "Please press any key to continue...",
-                               curses.color_pair(3))
-            self.screen.getkey()
-            curses.endwin()
+                    for each in ring_iterator:
+                        self.cursor_y += 1
+                        self.screen.addstr(self.cursor_y, 0, str(each),
+                                           curses.color_pair(2))
 
-    def from_zip(self, path):
-        self.screen.addstr(1, 0, "Please input a file member:",
+        self.screen.addstr(curses.LINES - 1, 0,
+                           "Please press any key to continue...",
                            curses.color_pair(3))
-        member = self.screen.getstr()
-        return ZipReader(path, member).next()
-
-    def from_txt_csv(self, path):
-        reader = ReaderFactory.get_reader(self.file_type)
-        if reader:
-            if next(reader(path)) is None:
-
-                return None
-
-            return reader(path)
-
-        return None
+        self.screen.getkey()
+        curses.endwin()
 
 
 if __name__ == '__main__':
-    interface = TextUiForm()
-    interface.get_data()
+    interface = UiForm()
+    interface.show_ring()
